9aafbaeeb3e1ec786279ff72842914bb646ee067
Matthew Wilhelm <matthew.wilhelm@uconn.edu>
2018-10-10 22:47:48 -0400

    Further Update

--- src/Optimizer/Relaxations/RelaxModel.jl
+++ src/Optimizer/Relaxations/RelaxModel.jl
@@ -4,21 +4,10 @@
 """
 function RelaxModel!(src::Optimizer,trg::T,n::NodeBB,r::RelaxationScheme; load::Bool = false) where {T<:MOI.AbstractOptimizer}
     if load
-        RelaxLinear!(src,trg)NLPData
+        RelaxLinear!(src,trg)
         trg.d = Build_NLP_Evaluator(src.NLPData.evaluator)
     else
         RelaxQuadratic!(src,trg,n,r,load)
         RelaxNonlinear!(src,trg,n,r,load)
     end
 end
-
-
-
-function RelaxNonlinear!(src::Optimizer,trg::T,r::RelaxationScheme, load::Bool) where {T<:MOI.AbstractOptimizer}
-    for i in NonlinearRelaxedAfterLoad
-    end
-end
-
-function RelaxLinear(m::Optimizer)
-end
-# DefaultRelaxationScheme()
